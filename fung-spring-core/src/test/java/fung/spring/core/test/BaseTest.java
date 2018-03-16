d2d6db7386700a41d43dcd808a4a1de01558714e
SDT14237 <783144350@qq.com>
2018-03-16 14:03:34 +0800

    Initial commit

---
+++ fung-spring-core/src/test/java/fung/spring/core/test/BaseTest.java
@@ -0,0 +1,12 @@
+package fung.spring.core.test;
+
+import org.junit.Test;
+
+public class BaseTest {
+
+    @Test
+    public void testHello() {
+        System.out.println("Hello Spring Core");
+    }
+
+}
