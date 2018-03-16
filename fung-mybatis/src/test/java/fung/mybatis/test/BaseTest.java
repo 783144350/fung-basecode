3e7dd09cd32db2dab713e077397e27ec265d819b
SDT14237 <783144350@qq.com>
2018-03-16 14:27:12 +0800

    init my base-code: fung-apache-commons

---
+++ fung-mybatis/src/test/java/fung/mybatis/test/BaseTest.java
@@ -0,0 +1,12 @@
+package fung.mybatis.test;
+
+import org.junit.Test;
+
+public class BaseTest {
+
+    @Test
+    public void testHello() {
+        System.out.println("Hello Mybatis");
+    }
+
+}
