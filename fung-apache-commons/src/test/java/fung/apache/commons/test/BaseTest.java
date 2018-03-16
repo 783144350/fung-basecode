906e6fa1e38cc8b53ecad8f76aa737d07ee63519
SDT14237 <783144350@qq.com>
2018-03-16 14:23:49 +0800

    init my base-code: fung-apache-commons

---
+++ fung-apache-commons/src/test/java/fung/apache/commons/test/BaseTest.java
@@ -0,0 +1,12 @@
+package fung.apache.commons.test;
+
+import org.junit.Test;
+
+public class BaseTest {
+
+    @Test
+    public void testHello() {
+        System.out.println("Hello Apache Commons");
+    }
+
+}
