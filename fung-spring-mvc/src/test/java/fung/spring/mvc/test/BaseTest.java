cce57d6548b1a77572160e372287b6d13923d776
SDT14237 <783144350@qq.com>
2018-03-16 14:09:39 +0800

    init my base-code

---
+++ fung-spring-mvc/src/test/java/fung/spring/mvc/test/BaseTest.java
@@ -0,0 +1,12 @@
+package fung.spring.mvc.test;
+
+import org.junit.Test;
+
+public class BaseTest {
+
+    @Test
+    public void testHello() {
+        System.out.println("Hello Spring MVC");
+    }
+
+}
