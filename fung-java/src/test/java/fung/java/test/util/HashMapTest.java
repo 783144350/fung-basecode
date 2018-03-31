12fc0c4b12fce792b6f8d3fb1d97aece51d36e09
fun <783144350@qq.com>
2018-03-31 11:45:39 +0800

    study java

---
+++ fung-java/src/test/java/fung/java/test/util/HashMapTest.java
@@ -0,0 +1,15 @@
+package fung.java.test.util;
+
+import org.junit.Test;
+
+import java.util.HashMap;
+
+public class HashMapTest {
+
+    @Test
+    public void testHello() {
+        //HashMap
+        System.out.println("Hello HashMap");
+    }
+
+}
